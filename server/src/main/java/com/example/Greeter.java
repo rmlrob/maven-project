a4506bee710f568ad19796434d6d3c1107e0dd0e
Rob Lewis <rml@ashfieldcontractservices.co.uk>
2017-08-10 12:08:56 +0100

    Fix style

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,7 +15,7 @@ public class Greeter {
   /**
    * @param someone is name of person
    * @return greeting string
-   */ 
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,4 +1,4 @@
-/** 
+/**
  * This is greeting package info.
  */
 package com.example;
